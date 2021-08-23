96a5a502d6fb5adbc34e92d1779cb0fcfb721242
Anatoly Silko <silko.a@gmail.com>
2021-08-23 09:09:49 +0100

    Service Provider

--- src/VaultServiceProvider.php
+++ src/VaultServiceProvider.php
@@ -7,11 +7,19 @@ use Illuminate\Support\ServiceProvider;
 class VaultServiceProvider extends ServiceProvider
 {
 
-    public function boot() {
+    public function boot () {
+
+        $router = $this->app['router'];
 
     }
 
-    public function register() {
+    public function register () {
+
+        $this->app->make('0notole\Vault\Controllers\AuthController');
+        $this->app->make('0notole\Vault\Controllers\MainController');
+        $this->app->make('0notole\Vault\Controllers\ResourceController');
+
+        $this->loadViewsFrom(__DIR__ . '/views', 'vault');
 
     }
 
