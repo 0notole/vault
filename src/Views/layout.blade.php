3e6e16d9cdfc990c9435acab5a210a1430c052b4
Anatoly Silko <silko.a@gmail.com>
2021-08-23 09:09:55 +0100

    Views

--- src/Views/layout.blade.php
+++ src/Views/layout.blade.php
@@ -2,11 +2,16 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>{{ config('app.name') }} | @yield('title', 'Vault Admin')</title>
     </head>
     <body>
 
+        @include('vault::partial.header')
+
         @yield('content')
+
+        @include('vault::partial.footer')
 
         @stack('scripts')
 
